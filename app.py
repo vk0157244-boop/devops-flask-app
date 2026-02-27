4513772edbf5be5070904f42ef12cfe8ef042939
singh <singh@gmail.com>
2026-02-27 14:12:37 +0000

    new chnge

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "DevOps CI/CD Project Live"
-    return "Webhook Automation Workingfine"
+    return "Webhook Automation Working"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
